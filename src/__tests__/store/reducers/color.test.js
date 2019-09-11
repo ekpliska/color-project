1a216cb7ed0dd99400387c1486655d1ee3c88c62
ekpliska <pliska.ek@gmail.com>
2019-09-11 22:19:28 +0300

    added: test reducer

--- src/__tests__/store/reducers/color.test.js
+++ src/__tests__/store/reducers/color.test.js
@@ -23,6 +23,28 @@ describe('color Reducer', () => {
             })
     })
 
-    it('RATE_COLOR success', () => {})
+    it('RATE_COLOR success', () => {
+        const state = {
+            id: 0,
+            title: 'Test Teal',
+            color: '#90C3D4',
+            timestamp: 'Sat Mar 12 2016 16:12:00 GMT-0800 (PST)',
+            rating: undefined
+        }
+        const action = {
+            type: C.RATE_COLOR,
+            id: 0,
+            rating: 3
+        }
+        const results = color(state, action);
+        expect(results)
+            .toEqual({
+                id: 0,
+                title: 'Test Teal',
+                color: '#90C3D4',
+                timestamp: 'Sat Mar 12 2016 16:12:00 GMT-0800 (PST)',
+                rating: 3
+            })
+    })
     
 })
--- src/reducers/colors.js
+++ src/reducers/colors.js
@@ -1,4 +1,4 @@
-import C from '../constants'
+import C from '../constants';
 
 export const color = (state = {}, action) => {
     switch (action.type) {
