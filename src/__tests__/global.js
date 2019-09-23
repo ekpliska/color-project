aa96cb03f8acf8f82f44281e219206edd47bea3b
ekpliska <pliska.ek@gmail.com>
2019-09-23 21:03:38 +0300

    added: test

---
+++ src/__tests__/global.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+global.React = React;
+global._testColors = [
+    {
+        id: "48498",
+        title: "lawn",
+        color: "#44ef37",
+        timestamp: "Mon Apr 11 2019 12:54:01 GTM-0700 (PDT)",
+        rating: 4
+    },
+    {
+        id: "48499",
+        title: "ocean blue",
+        color: "#0061ff",
+        timestamp: "Mon Apr 11 2019 12:54:01 GTM-0700 (PDT)",
+        rating: 2
+    },
+];
